e124f9e3515f106d9803b819467f4b7c979518db
elventian <elventian@gmail.com>
2020-07-09 16:33:07 +0300

    Reworked using std::regex

--- app/main.cpp
+++ app/main.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
+#include <regex>
+#include <string>
 #include "httplib.h"
 
 int main(int argc, char* argv[])
 {
-    std::string server_url(argv[1]);
-    std::string player_key(argv[2]);
+	const std::string serverUrl(argv[1]);
+	const std::string playerKey(argv[2]);
 
-    std::cout << "ServerUrl: " << server_url << "; PlayerKey: " << player_key << std::endl;
+	std::cout << "ServerUrl: " << serverUrl << "; PlayerKey: " << playerKey << std::endl;
+	
+	const std::regex urlRegexp("http://(.+):(\\d+)");
+	std::smatch urlMatches;
+	if (!std::regex_search(serverUrl, urlMatches, urlRegexp) || urlMatches.size() != 3) {
+		std::cerr << "Bad server url" << std::endl;
+		return 1;
+	}
+	const std::string serverName = urlMatches[1];
+	const int serverPort = std::stoi(urlMatches[2]);
+	httplib::Client client(serverName, serverPort);
+	const std::shared_ptr<httplib::Response> serverResponse = 
+		client.Get((serverUrl + "?playerKey=" + playerKey).c_str());
 
-    auto url = server_url.substr(server_url.find(":") + 3); // Remove schema
-    auto colonPosition = url.find(":");
-    auto pathPosition = url.find("/") != std::string::npos ? url.find("/") : url.length();
-    auto host = url.substr(0, colonPosition);
-    auto port = std::stoi(url.substr(colonPosition + 1, pathPosition));
-    auto path = url.substr(pathPosition);
+	if (!serverResponse) {
+		std::cerr << "No response from server" << std::endl;
+		return 2;
+	}
+	
+	if (serverResponse->status != 200) {
+		std::cerr << "Server returned error: " <<
+			httplib::detail::status_message(serverResponse->status) << 
+			" (" << serverResponse->status << ")" << std::endl;
+		return 3;
+	}
 
-    httplib::Client cli(host, port);
-
-    auto res = cli.Get((path + "?playerKey=" + player_key).c_str());
-
-    if (!res || res->status != 200) {
-        return 1;
-    }
-
-    return 0;
+	return 0;
 }
+
