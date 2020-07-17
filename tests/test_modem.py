2dbae73c7ba31917df25a21f635171bc70fbcfca
Philipp Joram <mail@phijor.me>
2020-07-17 23:44:46 +0200

    Add mod/demod functions

---
+++ tests/test_modem.py
@@ -0,0 +1,48 @@
+import pytest
+
+from zebv.modem import mod, demod, mod_num
+
+MODULATED_TO_DEMODULATED = [
+    ("010", 0),
+    ("01100001", 1),
+    ("00", ()),
+    ("110000", ((), ())),
+    ("1101000", (0, ())),
+    ("110110000101100010", (1, 2)),
+    ("1101100001110110001000", (1, (2, ()))),
+]
+
+
+@pytest.mark.parametrize("input, expected", MODULATED_TO_DEMODULATED)
+def test_demod(input, expected):
+    assert demod(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected", list(map(lambda e: (e[1], e[0]), MODULATED_TO_DEMODULATED))
+)
+def test_mod(input, expected):
+    assert mod(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (0, "010"),
+        (1, "01100001"),
+        (-1, "10100001"),
+        (2, "01100010"),
+        (-2, "10100010"),
+        (15, "01101111"),
+        (-15, "10101111"),
+        (16, "0111000010000"),
+        (-16, "1011000010000"),
+        (255, "0111011111111"),
+        (-255, "1011011111111"),
+        (256, "011110000100000000"),
+        (-256, "101110000100000000"),
+    ],
+)
+def test_mod_num(caplog, input, expected):
+    with caplog.at_level("DEBUG"):
+        assert mod_num(input) == expected
---
+++ zebv/modem.py
@@ -0,0 +1,79 @@
+from typing import Union, Tuple
+
+from math import ceil
+
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
+def mod_num(input: int) -> str:
+    abs_val = int(abs(input))
+    if abs_val == 0:
+        return "010"
+    else:
+        pos_or_neg = "01" if input >= 0 else "10"
+        bit_len = abs_val.bit_length()
+
+        n = ceil(bit_len / 4)
+
+        unary_len = "1" * n + "0"
+
+        logger.debug(f"{input=} {unary_len=} {n=} {bit_len=}")
+
+        return f"{pos_or_neg}{unary_len}{abs_val:0{4*n}b}"
+
+
+def mod(input) -> str:
+    if isinstance(input, int):
+        return mod_num(input)
+    elif isinstance(input, tuple):
+        if len(input) == 0:
+            return "00"
+        elif len(input) == 2:
+            head = mod(input[0])
+            tail = mod(input[1])
+            return f"11{head}{tail}"
+    else:
+        raise TypeError(f"The fuck is this? {input=}")
+
+
+def demod_num(input: str) -> (int, str):
+    bits = 0
+    while True:
+        (fst, input) = input[0], input[1:]
+        if fst == "1":
+            bits += 1
+        else:
+            break
+
+    if not bits:
+        return (0, input)
+    else:
+        value_len = bits * 4
+        (value, rest) = input[:value_len], input[value_len:]
+        return (int(value, base=2), rest)
+
+
+def demod(input: str) -> Union[Tuple, int]:
+    def demod_impl(input: str) -> (Union[Tuple, int], str):
+        (type_, value) = (input[:2], input[2:])
+
+        if type_ == "00":
+            return ((), value)
+        elif type_ == "01":
+            (num, rest) = demod_num(value)
+            return (1 * num, rest)
+        elif type_ == "10":
+            (num, rest) = demod_num(value)
+            return (-1 * num, rest)
+        elif type_ == "11":
+            (head, tail) = demod_impl(value)
+            (tail, rest) = demod_impl(tail)
+            return ((head, tail), rest)
+        else:
+            raise ValueError(f"Unexpected type {type_!r}")
+
+    (result, rest) = demod_impl(input)
+    print(f"demod_impl({input=}) => {result=} {rest=}")
+    return result
