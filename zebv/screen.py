6e6caae4dfbfa8a990c2581cb42fc3e76f7b94ce
Andreas Gocht <andreas.gocht@tu-dresden.de>
2020-07-18 17:04:47 +0200

    screen + draw

--- zebv/screen.py
+++ zebv/screen.py
@@ -10,11 +10,13 @@ class AlienScreen(Thread):
         super().__init__()
         # Load the font, a 64 by 8 tile font with libtcod's old character layout.
         self.tileset = tcod.tileset.load_tilesheet(
-            "dejavu10x10_gs_tc.png", 64, 8, tcod.tileset.CHARMAP_TCOD,
+            "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD,
         )
         # Create the main console.
         self.console = tcod.Console(WIDTH, HEIGHT)
         self.points = []
+        self._max_x = 20
+        self._max_y = 20
 
     def run(self):
         with tcod.context.new_terminal(
@@ -37,8 +39,15 @@ class AlienScreen(Thread):
                     if event.type == "QUIT":
                         raise SystemExit()
 
-    def draw(self, points):
-        self.points += points
+    def draw(self, nodes=[]):
+        points = []
+        for n in nodes:
+            assert n.op.op.name == "cons"
+            x = n.op.arg
+            y = n.arg
+            points.append((x.value, y.value))
+            self._max_x = max(x.value + 1, self._max_x)
+            self._max_y = max(y.value + 1, self._max_y)
 
     def clean(self):
         self.points = []
