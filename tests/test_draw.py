c1d0b8d2387d411ca7fc152f69cd602db25ee107
tilsche <thomas.ilsche@tu-dresden.de>
2020-07-18 08:00:39 +0200

    format draw because someone doesn't black

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -1,9 +1,11 @@
 import pytest
-import zebv.parsing as parsing
 import zebv.draw as draw
+import zebv.parsing as parsing
+
 
 def t(exp):
     return parsing.build_expression(parsing.tokenize(exp))
+
 
 def t_list(vecs):
     res = []
@@ -11,26 +13,34 @@ def t_list(vecs):
         res.append(t(v))
     return res
 
+
 def test_draw_1():
     draw.draw()
+
 
 def test_draw_2():
     draw.draw(t_list("ap ap vec 1 1"))
 
+
 def test_draw_3():
     draw.draw(t_list("ap ap vec 1 2"))
+
 
 def test_draw_4():
     draw.draw(t_list("ap ap vec 2 5"))
 
+
 def test_draw_5():
     draw.draw(t_list("ap ap vec 1 2 , ap ap vec 3 1 "))
-    
-def test_draw_6():
-    draw.draw(t_list("ap ap vec 5 3 , ap ap vec 6 3 , ap ap vec 4 4 , ap ap vec 6 4 , ap ap vec 4 5"))
 
-    
-    
+
+def test_draw_6():
+    draw.draw(
+        t_list(
+            "ap ap vec 5 3 , ap ap vec 6 3 , ap ap vec 4 4 , ap ap vec 6 4 , ap ap vec 4 5"
+        )
+    )
+
 
 if __name__ == "__main__":
     test_draw_6()
--- zebv/draw.py
+++ zebv/draw.py
@@ -1,25 +1,26 @@
-from . import node
-from PIL import Image
 import numpy as np
+from PIL import Image
+
+from . import node
+
 
 class Img:
     def __init__(self):
-        self._size = (13,17)
+        self._size = (13, 17)
         self.pixels = np.zeros(self._size, np.uint8)
-
 
     def save(self, filename):
         self._img = Image.fromarray(self.pixels, mode="L")
         self._img.save(filename, format="png")
 
 
-def draw(nodes = [], filename = "test.png"):
+def draw(nodes=[], filename="test.png"):
     im = Img()
     for n in nodes:
         assert n.op.op.name == "vec"
         y = n.op.arg
         x = n.arg
-        im.pixels[x.value,y.value] = 255
+        im.pixels[x.value, y.value] = 255
     print(im.pixels)
     im.save(filename)
-        #im.pixels
+    # im.pixels
