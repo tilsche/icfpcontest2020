56afb225931e579e3449d4e3a7a7c6747faee0b2
Philipp Joram <mail@phijor.me>
2020-07-18 15:54:59 +0200

    Move app => zebv/app, update setup.py and wheels

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pip install --no-index --find-links ./wheels icfpcontest2020
+pip install --no-index --find-links ./wheels zebv
 echo "HI!"
 pwd
 ls -lha
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="icfpcontest2020",
+    name="zebv",
     version="0.1",
     author="Zebra Infused Hamsters",
     python_requires=">=3.7",
-    packages=["app", "zebv"],
+    packages=["zebv"],
     scripts=[],
     entry_points="""
       [console_scripts]
-      py-zebra=app:main
+      py-zebra=zebv.app:main
       """,
     install_requires=["requests", "pillow", "click", "click_log"],
     tests_require=["pytest", "tcod"],
