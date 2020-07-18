4b64ddb615225352d85c065489bb21908afd08f8
Mario Bielert <mario.bielert@googlemail.com>
2020-07-18 17:03:14 +0200

    Update screen to nice pixel

--- zebv/screen.py
+++ zebv/screen.py
@@ -2,15 +2,16 @@ from threading import Thread
 
 import tcod
 
-WIDTH, HEIGHT = 80, 60  # Console width and height in tiles.
+WIDTH, HEIGHT = 20, 20  # Console width and height in tiles.
+
+PIXEL = 9565
 
 
 class AlienScreen(Thread):
     def __init__(self):
         super().__init__()
-        # Load the font, a 64 by 8 tile font with libtcod's old character layout.
         self.tileset = tcod.tileset.load_tilesheet(
-            "dejavu10x10_gs_tc.png", 64, 8, tcod.tileset.CHARMAP_TCOD,
+            "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD,
         )
         # Create the main console.
         self.console = tcod.Console(WIDTH, HEIGHT)
@@ -27,18 +28,22 @@ class AlienScreen(Thread):
                 self.console.clear()
 
                 for (x, y) in self.points:
-                    self.console.put_char(x, y, ch=75)
+                    self.console.draw_rect(x, y, 1, 1, ch=PIXEL)
 
-                context.present(self.console)
+                context.present(self.console, keep_aspect=True)
 
                 for event in tcod.event.wait():
                     context.convert_event(event)
-                    # print(event)
+                    if event.type == "MOUSEBUTTONDOWN":
+                        self.on_mouse_click(*event.tile)
                     if event.type == "QUIT":
                         raise SystemExit()
 
     def draw(self, points):
         self.points += points
 
-    def clean(self):
+    def clear(self):
         self.points = []
+
+    def on_mouse_click(self, x, y):
+        print(f"mouse click on ({x}, {y})")
