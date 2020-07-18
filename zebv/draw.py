7f81edb843a513d7a9794a6f2631fdb4df001d45
Andreas Gocht <andreas.gocht@tu-dresden.de>
2020-07-18 14:05:43 +0200

    add Picture, with multible draws

--- zebv/draw.py
+++ zebv/draw.py
@@ -14,26 +14,59 @@ class Img:
         self._img.save(filename, format="png")
 
 
-
-def draw(nodes=[], filename="", size = (1,1)):
+def draw_sub_image(nodes=[], filename="", size=(1, 1)):
     points = []
     max_x = size[0]
     max_y = size[1]
     for n in nodes:
         assert n.op.op.name == "cons"
-        y = n.op.arg
-        x = n.arg
-        points.append((x.value,y.value))
-        max_x = max(x.value + 1,max_x)
-        max_y = max(y.value + 1,max_y)
+        x = n.op.arg
+        y = n.arg
+        points.append((x.value, y.value))
+        max_x = max(x.value + 1, max_x)
+        max_y = max(y.value + 1, max_y)
 
     new_size = (max_x, max_y)
-    im = Img(new_size)        
+    im = Img(new_size)
 
-    for point in points: 
+    for point in points:
         im.pixels[point] = 255
 
     if filename:
         im.save(filename)
 
     return im.pixels
+
+
+class Picture:
+    def __init__(self, size=(1, 1)):
+        self._size_x = size[0]
+        self._size_y = size[1]
+
+        self._pixels = np.zeros(size, np.uint8)
+        self._current_x = 0
+        self._current_y = 0
+        self._border = 2
+
+    def draw(self, nodes=[], size=(1, 1)):
+        sub_pixels = draw_sub_image(nodes, size=size)
+        (sub_x, sub_y) = sub_pixels.shape
+
+        self._size_x = max(sub_x + self._current_x + self._border, self._size_x)
+        self._size_y = max(sub_y + self._current_y + self._border, self._size_y)
+
+        self._pixels.resize((self._size_x + 2, self._size_y + 2))
+        self._pixels[
+            self._current_x + self._border : self._current_x + self._border + sub_x,
+            self._current_y + self._border : self._current_y + self._border + sub_y,
+        ] = sub_pixels
+
+        self._current_x = self._current_x + self._border + sub_x
+        # no check for linebreaks yet, self.current_y stays 0
+        # self._current_y = self._current_y + self._border + sub_y
+
+    def save(self, filename=""):
+        self._img = Image.fromarray(np.transpose(self._pixels), mode="L")
+        print(filename)
+        self._img.save(filename, format="png")
+
