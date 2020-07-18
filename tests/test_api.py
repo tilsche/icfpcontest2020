f78ad959d69ff8d96bd48b1b55fa77b3a251ee84
Philipp Joram <mail@phijor.me>
2020-07-18 13:08:44 +0200

    Test Interaction with a live ApiClient

---
+++ tests/test_api.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+from logging import getLogger, DEBUG
+
+from zebv.api import ApiClient
+from zebv.interact import Interaction
+
+logger = getLogger(__name__)
+
+
+@pytest.fixture
+def api_client():
+    yield ApiClient("https://icfpc2020-api.testkontur.ru/", os.environ["PLAYER_KEY"])
+
+
+def test_interact(caplog, api_client, statefuldraw):
+    with caplog.at_level(DEBUG):
+        logger.debug(f"Connected to {api_client.base_url}")
+        i = Interaction(
+            statefuldraw, "statefuldraw", send_function=api_client.aliens_send
+        )
+        i(4, 8)
+        i(7, 7)
+        i(12, 14)
