c571b3350886e81403ce4b95cc445bc8bae1c74d
Philipp Joram <mail@phijor.me>
2020-07-18 15:47:36 +0200

    Install zebv with package

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version="0.1",
     author="Zebra Infused Hamsters",
     python_requires=">=3.7",
-    packages=["app"],
+    packages=["app", "zebv"],
     scripts=[],
     entry_points="""
       [console_scripts]
       py-zebra=app:main
       """,
     install_requires=["requests", "pillow", "click", "click_log"],
-    tests_requires=["pytest", "tcod"],
+    tests_require=["pytest", "tcod"],
     extras_require={"tests": ["pytest", "tcod"]},
 )
