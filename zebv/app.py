7371870da290bf7d17ea2251b265242b9b68898e
Andreas Gocht <andreas.gocht@tu-dresden.de>
2020-07-20 12:23:47 +0200

    raise ramdom act

--- zebv/app.py
+++ zebv/app.py
@@ -309,9 +309,11 @@ class DefendPlayer(Player):
                         and self.game_response.game_state.game_tick > 3
                     ):
                         # just do some random navigation, as long as we are far away
-                        if random.uniform(0, 1) < 0.25:  # only with a chance of 1/4
+                        if random.uniform(0, 1) < 0.5:  # only with a chance of 1/4
                             vec = random.choice([(1, 1), (-1, -1), (1, 0), (0, 1)])
-                            self.log.info(f"RANDOM ACCELERATE {ship.ship_id}, VEC: {vec}")
+                            self.log.info(
+                                f"RANDOM ACCELERATE {ship.ship_id}, VEC: {vec}"
+                            )
                             self.game_response = self.accelerate(ship.ship_id, vec)
                             continue
 
